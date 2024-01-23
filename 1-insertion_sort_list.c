43e074c09d02e5183af37267d7de339c25c16147
abdelrhman892 <abdoayman5m@gmail.com>
2024-01-23 09:19:01 -0800

    fixing

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  *swap - swaps 2 nodes in a doubly-linked list
  *@a: address of first node
@@ -6,41 +7,42 @@
 */
 void swap(listint_t *a, listint_t *b)
 {
-    if (a->prev)
-        a->prev->next = b;
-    if (b->prev)
-        b->next->prev = a;
-    a->next = b->next;
-    b->prev = a->prev;
-    a->prev = b;
-    b->next = a;
+	if (a->prev)
+		a->prev->next = b;
+	if (b->prev)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
 }
+
 /**
  *insertion_sort_list - insertion sorts a doubly-linked list
  * @list: address of pointer to head node
 */
 void insertion_sort_list (listint_t **list)
 {
-    listint_t *i, *j;
+	listint_t *i, *j;
 
-    if (!list || !*list || !(*list) ->next)
-        return;
-    i = (*list)->next;
-    while (i)
-    {
-        j = i;
-        i = i->next;
-        while (j && j->prev)
-        {
-            if (j->prev->n > j->n)
-            {
-                swap (j->prev, j);
-                if (!j->prev)
-                    *list = j;
-                print_list((const listint_t *) *list);
-            }
-            else
-                j = j->prev;
-        }
-    }
+	if (!list || !*list || !(*list) ->next)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+		while (j && j->prev)
+		{
+			if (j->prev->n > j->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *) *list);
+			}
+			else
+				j = j->prev;
+		}
+	}	
 }
